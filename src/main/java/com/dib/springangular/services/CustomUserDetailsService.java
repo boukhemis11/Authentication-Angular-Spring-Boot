c74a5d019d10028b4f3482cfc570e287877513e9
34011-12-12 <34011-12-12@exchange.ad.afpanet>
2019-07-23 10:48:10 +0200

    add service projet

---
+++ src/main/java/com/dib/springangular/services/CustomUserDetailsService.java
@@ -0,0 +1,55 @@
+package com.dib.springangular.services;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.dib.springangular.models.Role;
+import com.dib.springangular.models.User;
+import com.dib.springangular.repositories.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+	private UserRepository userRepository;
+    
+    public User findUserByEmail(String email) {
+	    return userRepository.findByEmail(email);
+	}
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        if(user != null) {
+	        List<GrantedAuthority> authorities = getUserAuthority(user.getRoles());
+	        return buildUserForAuthentication(user, authorities);
+	    } else {
+	        throw new UsernameNotFoundException("username not found");
+	    }
+    }
+
+    private List<GrantedAuthority> getUserAuthority(Set<Role> userRoles) {
+	    Set<GrantedAuthority> roles = new HashSet<>();
+	    userRoles.forEach((role) -> {
+	        roles.add(new SimpleGrantedAuthority(role.getRole()));
+	    });
+
+	    List<GrantedAuthority> grantedAuthorities = new ArrayList<>(roles);
+	    return grantedAuthorities;
+	}
+	
+	private UserDetails buildUserForAuthentication(User user, List<GrantedAuthority> authorities) {
+	    return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), authorities);
+	}
+
+}
